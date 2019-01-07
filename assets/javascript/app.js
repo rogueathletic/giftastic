26ef72ce1923645b0df9a650bdf2b4b9ce0d9a29
Jason Schutz <jason@rogueathletic.com>
2019-01-06 19:10:27 -0800

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+
+var gifs = ["parkor fail", "wtf", "pogo fail", "bicycle fail", "kook slam", "cool", "weird", "random", "high five"];
+function makeButtons(){    /*    */
+	$('#buttonsView').empty();
+	for (var i = 0; i < gifs.length; i++){
+		var a = $('<button>') 
+		a.addClass('gif-button'); 
+		a.attr('data-name', gifs[i]); 
+		a.text(gifs[i]);
+		$( '#buttonsView' ).append (a);
+	}
+}
+$( "#addgif" ).on( "click", function (){
+
+	var gif = $( "#gif-input" ).val ().trim ();
+	gifs.push ( gif );
+	makeButtons ();
+	return false; 
+})
+
+
+function displayGifs() {
+	var gif = $( this ).attr( "data-name" );
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&limit=20&api_key=dc6zaTOxFJmzC";
+
+		$.ajax( {url: queryURL, method: "GET" } ).done( function ( response ) {
+			console.log(response.data);
+			var results = response.data;
+			for ( var i = 0; i < results.length; i++ ) {
+				  var gifDiv = $       ( '<div class=gifs>'                      );
+			 	  var gifGif = $       ( '<img>'                                 );
+					  gifGif.attr      ( 'src', 
+									   results[i].images.fixed_height_still.url  );
+					  gifGif.attr      ( 'title', "Rating: " + 
+									 results[i].rating                           );
+					  gifGif.attr      ( 'data-still', 
+					                 results[i].images.fixed_height_still.url    );
+					  gifGif.attr      ( 'data-state', 'still'                   );
+					  gifGif.addClass  ( 'gif'                                   );
+					  gifGif.attr      ( 'data-animate', 
+					                  results[i].images.fixed_height.url         );
+	
+					gifDiv.prepend( gifGif )
+				$( "#gifsView" ).prepend( gifDiv );
+			}
+		});
+	
+
+}
+$(document).on('click', '.gif', function(){
+	var state = $(this).attr('data-state');
+		if ( state == 'still'){
+                $(this).attr('src', $(this).data('animate'));
+                $(this).attr('data-state', 'animate');
+            }else{
+                $(this).attr('src', $(this).data('still'));
+                $(this).attr('data-state', 'still');
+            };
+});
+$(document).on("click",".gif-button", displayGifs);
+makeButtons();
+
